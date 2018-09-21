d29fd22c89ea027282d3c8d8c511ae58a774cb80
David Carrillo <iscdavidcarrillo@gmail.com>
2018-09-21 13:59:53 -0700

    Implementing components with props and state

--- src/App.js
+++ src/App.js
@@ -5,11 +5,31 @@ import Person from './Person/Person.js';
 import FuncPerson  from './Person/FuncPerson.js';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'David', age: 41},
+      { name: 'Zonia', age: 45},
+      { name: 'Nancy', age: 14}
+    ]
+  }
+
+  switchNameHandler = () => {
+    this.setState({persons: [
+      { name: 'Arturo', age: 41},
+      { name: 'Zonia', age: 45},
+      { name: 'Nancy', age: 14}
+    ]
+  });
+  }
+
   render() {
     return (
       <div className="App">
           <h1>React Works!</h1>
-          <Person></Person>
+          <button onClick={this.switchNameHandler}>Switch  Name</button>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My hobbies are: Playing Violin and reading books</Person>
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
           <FuncPerson exclamation="OK"></FuncPerson>
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
 class Person extends Component{
+
   render(props){
     return (
-      <h1>This is a Person Component</h1>
+      <div>
+        <h1>I'm {this.props.name} and I'm {this.props.age} years old</h1>
+        <p>{this.props.children}</p>
+      </div>
     );
   }
 }
